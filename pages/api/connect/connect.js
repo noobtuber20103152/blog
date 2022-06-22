5eb020b2adb59491a7248d5a20290cef01b705e2
ujjawal <ujjawalrachhoya100@gmail.com>
2022-06-23 01:37:04 +0530

    cloud mongodb atlas

--- pages/api/connect/connect.js
+++ pages/api/connect/connect.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/authentication?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false" //MongoDB uri 
-const connectToMongo = () => {
-    mongoose.connect(mongoURI, () => {
-        console.log("Connected to mongo successfully")
-    })
+const connection = {};
+const uri = "mongodb+srv://noobtuber:noobtuber@cluster0.lynapyy.mongodb.net/?retryWrites=true&w=majority"
+async function connectToMongo() {
+    if (connection.isConnected) {
+        return;
+    }
+
+    const db = await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    connection.isConnected = db.connections[0].readyState;
 }
+
 module.exports = connectToMongo;
