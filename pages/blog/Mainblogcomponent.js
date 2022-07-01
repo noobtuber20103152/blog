49bf2e7245c23f9b6697123849e5e95d67774be7
ujjawal <ujjawalrachhoya100@gmail.com>
2022-07-02 04:23:50 +0530

    social media share button

--- pages/blog/Mainblogcomponent.js
+++ pages/blog/Mainblogcomponent.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Image from "next/image"
+import { AiOutlineShareAlt } from "react-icons/ai"
+import { useState } from 'react';
+import Model from "./Model"
 function Mainblogcomponent(props) {
     const myLoader = ({ src }) => {
         return src;
@@ -11,16 +14,19 @@ function Mainblogcomponent(props) {
         <>
             <div className="container flex flex-col justify-center items-center my-20 ">
                 <div className='lg:w-7/12 w-11/12'>
-                    <div className='mb-10'>
+                    <div className='mb-5'>
                         <h1 className='text-base text-gray-600 font-medium text-center '>{props.author}</h1>
                         <h1 className='text-base text-gray-600  text-center '>{props.createdAt}</h1>
                     </div>
-                    <div className="my-10 flex flex-col justify-center items-center">
+                    <div className='absolute -my-5 text-center py-0 '>
+                        <Model/>
+                    </div>
+                    <div className="my-5 flex flex-col justify-center items-center">
                         <h1 className='md:text-5xl text-4xl text-center font-bold'>{props.title}</h1>
                         <p className='text-center w-full md:w-10/12 text-2xl fontsemibold text-gray-600 mt-5' >{props.shortdesc}</p>
                     </div>
                     <div className="mt-5">
-                        <Image loader={myLoader} className="w-full rounded-lg h-[34rem]" src={props.bgimage?props.bgimage:"asdf"} height={500} width={1000} />
+                        <Image loader={myLoader} className="w-full rounded-lg h-[34rem]" src={props.bgimage ? props.bgimage : "asdf"} height={500} width={1000} />
                     </div>
                     <div className='mt-5'>
                         {props.longdesc && props.longdesc?.map((e) => {
@@ -36,7 +42,7 @@ function Mainblogcomponent(props) {
                             {props.otherimages && props.otherimages?.map((e) => {
                                 return <>
                                     <div className="w-full rounded">
-                                        <Image loader={loder} className="w-96 hover:scale-105 duration-500 hover:cursor-pointer h-96 rounded-lg " src={e?e:"asdfa"} height={600} width={1000} />
+                                        <Image loader={loder} className="w-96 hover:scale-105 duration-500 hover:cursor-pointer h-96 rounded-lg " src={e ? e : "asdfa"} height={600} width={1000} />
                                     </div>
                                 </>
                             })}
---
+++ pages/blog/Model.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { AiOutlineShareAlt, AiFillFacebook, AiFillLinkedin, AiOutlineWhatsApp } from "react-icons/ai"
+import { BsTwitter } from "react-icons/bs"
+function Model() {
+    const [display, setdisplay] = useState("hidden")
+    const onclick1 = () => {
+        if (display == "hidden") {
+            setdisplay("block");
+        }
+        else {
+            setdisplay("hidden");
+        }
+    }
+    const onchange = () => {
+        return;
+    }
+    return <>
+        <AiOutlineShareAlt className='hover:cursor-pointer text-center text-3xl' onClick={onclick1} />
+        <div className={`${display} overflow-y-auto md:w-7/12 w-10/12 h-auto border shadow-lg bg-white `}>
+            <div className='flex flex-wrap justify-between '>
+                <a href={`https://twitter.com/share?url=${window.location.href}`} target="_blank" className="w-6/12 flex justify-start items-center px-2 py-4 hover:bg-[#f2f4f7]">
+                    <BsTwitter className='text-2xl text-blue-500' />
+                    <span className='mx-2 text-lg font-semibold' >Twitter</span>
+                </a>
+                <a href={`whatsapp://send?text=${window.location.href}`} target="_blank" className="w-6/12 flex justify-start items-center px-2 py-4 hover:bg-[#f2f4f7]">
+                    <AiOutlineWhatsApp className='text-2xl text-green-700' />
+                    <span className='mx-2 text-lg font-semibold' >WhatsApp</span>
+                </a>
+
+                 <a href={`https://linkedin.com/shareArticle?url=${window.location.href}`} target="_blank" className="w-6/12 flex justify-start items-center px-2 py-4 hover:bg-[#f2f4f7]">
+                   <AiFillLinkedin className='text-2xl text-blue-600' />
+                    <span className='mx-2 text-lg font-semibold' >LinkedIn</span>
+                </a> 
+                <a href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`} target="_blank" className="w-6/12 flex justify-start items-center px-2 py-4 hover:bg-[#f2f4f7]">
+                    <AiFillFacebook className='text-2xl text-blue-900' />
+                    <span className='mx-2 text-lg font-semibold' >Facebook</span>
+                </a>
+            </div>
+            <div className='my-2' >
+                <hr />
+            </div>
+            <div className='my-0 flex justify-start flex-col px-4 pb-2'>
+                <p className='text-start mb-1' >blog page link</p>
+                <input onChange={onchange} value={window.location.href} onfocus="this.blur()" type="text" className='border rounded-sm text-sm bg-[#f2f4f7] w-12/12 outline-none py-3 px-2 ' />
+            </div>
+
+        </div>
+    </>
+}
+
+export default Model
