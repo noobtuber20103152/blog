db0fce5315023054704497804428ebe701d3e35d
ujjawal <ujjawalrachhoya100@gmail.com>
2022-07-02 06:20:55 +0530

    update

--- pages/components/cards/Hotizontalcard.js
+++ pages/components/cards/Hotizontalcard.js
@@ -1,12 +1,6 @@
 import React, { useState } from 'react'
 import Link from "next/link"
-import { HandThumbsUp, HandThumbsUpFill } from 'react-bootstrap-icons';
-import PreviousMap from 'postcss/lib/previous-map';
 function Hotizontalcard(props) {
-    const [like, setlike] = useState(false);
-    const onclick = () => {
-        setlike(!like);
-    }
     return (
         <>
             <div className='flex justify-center '>
@@ -17,14 +11,9 @@ function Hotizontalcard(props) {
                         </div>
                         <div className="lg:p-6 p-4 lg:w-9/12 flex justify-end  flex-col ">
                             <span className='text-base  font-medium text-gray-400 mb-3' > {props.author}  -  {props.createdAt}</span>
-
-                            <Link href={`../../blog/${props.title?props.title.split(" ").join("-"):"page-not-found"}`}><a className="text-gray-900 text-4xl lg:text-5xl w-full font-bold mb-22">{props.title}</a></Link>
+                            <Link href={`../../blog/${props.title ? props.title.split(" ").join("-") : "page-not-found"}`}><a className="text-gray-900 text-4xl lg:text-5xl w-full font-bold mb-22">{props.title}</a></Link>
                             <p className="text-gray-700 text-base w-12/12 font-medium  mt-2 mb-4">{props.shortdesc}
                             </p>
-                            <div className='flex'>
-                                {!like && <HandThumbsUp onClick={onclick} className='text-3xl hover:cursor-pointer' />}
-                                {like && <HandThumbsUpFill onClick={onclick} className='text-3xl hover:cursor-pointer' fill='black' />}
-                            </div>
                         </div>
                     </div>
                 </div>
--- pages/components/siderbar/Sidebar.js
+++ pages/components/siderbar/Sidebar.js
@@ -131,7 +131,7 @@ function Sidebar() {
                 </div>
                 <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 hover:text-white " >
                     {<BsCurrencyDollar className='inline' />}
-                    <button onClick={pay}  ><span className="text-[15px] ml-4  font-bold">Want to donate?</span></button>
+                    <button onClick={pay}  ><span className="text-[15px] ml-4  font-bold">Want to help me?</span></button>
 
                 </div>
                 <div>
