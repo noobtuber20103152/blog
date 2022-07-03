fd76ea0e96f8761be223726d2bf5fbda82fbe0f1
ujjawal <ujjawalrachhoya100@gmail.com>
2022-07-03 18:35:16 +0530

    loader

--- pages/components/slider/ImageSlider.js
+++ pages/components/slider/ImageSlider.js
@@ -3,6 +3,7 @@ import Hotizontalcard from '../cards/Hotizontalcard'
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { AiOutlineLoading3Quarters } from "react-icons/ai"
 function ImageSlider() {
     const [fetchdata, setfetchdata] = useState([]);
     const [count, setcount] = useState(0);
@@ -26,6 +27,9 @@ function ImageSlider() {
     return (
         <>
             <h1 className='text-4xl text-center font-bold mt-20'>Popular blogs</h1>
+            {fetchdata.length == 0 && <div className="flex justify-center items-center mt-10">
+                <AiOutlineLoading3Quarters className='text-2xl animate-spin' />
+            </div>}
             <Slider className='md:mx-7 mx-6' ref={slRef} {...settings} >
                 {(fetchdata).map((e) => {
                     return <>
